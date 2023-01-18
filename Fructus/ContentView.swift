bff9c809d4ded5e14a35a6e6dcb6c17354469e00
mohammedrashid <mohammed1rashid786@gmail.com>
2023-01-19 00:31:37 +0530

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Mohammed Rashid on 19/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mohammed Rashid on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
